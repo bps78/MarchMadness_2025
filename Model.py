acd0210c90bd785156196d9151fed47b78879e6f
bps78 <90796928+bps78@users.noreply.github.com>
2025-03-11 19:53:30 -0500

    See Previous

--- Model.py
+++ Model.py
@@ -33,13 +33,6 @@ output["Score"] = (output["Actual"] - output["Predictions"])**2
 # Score = 0.25 indicates each team is given an equal chance, no pattern association
 print(Fore.BLUE + "********** PREDITCTION SCORE: " + Fore.GREEN + str(output["Score"].mean()) + Fore.BLUE + " ***************" + Fore.WHITE)
 
-test['Pred'] = predictions[:,1]
-test['rounded_preds'] = np.round(predictions[:,1])
-test['Actual'] = (test['T1_Score'] > test['T2_Score']).astype(int)
-summary = test[['Season', 'DayNum', 'T1_TeamID', 'T1_Score', 'T2_TeamID', 'T2_Score', 'Pred', 'rounded_preds', 'Actual']]
-
-summary.to_csv('Predictions.csv', index=False)
-
 #Optimized parameters to avoid overfitting and improve the model
 param_grid = {
     'max_depth': [3, 5, 7],
@@ -62,4 +55,11 @@ output['Actual'] = ytest.astype(int).reset_index(drop=True)
 output["Score"] = (output["Actual"] - output["Predictions"])**2
 print(Fore.BLUE + "********** OPTIMIZED PREDITCTION SCORE: " + Fore.GREEN + str(output["Score"].mean()) + Fore.BLUE + " ***************" + Fore.WHITE)
 
+#Updated Predictions
+test['Pred'] = predictions2[:,1]
+test['rounded_preds'] = np.round(predictions2[:,1])
+test['Actual'] = (test['T1_Score'] > test['T2_Score']).astype(int)
+summary = test[['Season', 'DayNum', 'T1_TeamID', 'T1_Score', 'T2_TeamID', 'T2_Score', 'Pred', 'rounded_preds', 'Actual']]
 
+#Save our Predictions for viewing
+summary.to_csv('Predictions.csv', index=False)
