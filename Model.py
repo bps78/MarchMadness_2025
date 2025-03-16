7b215beabda6d06aad81aaff101817b95943bcf4
bps78 <90796928+bps78@users.noreply.github.com>
2025-03-15 22:09:59 -0500

    Fixed Detached Head

--- Model.py
+++ Model.py
@@ -1,41 +1,33 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from xgboost import XGBClassifier #Model used for prediction
-from sklearn.ensemble import RandomForestClassifier #Random forest for feature selection
-from colorama import Fore #Used to print colored console output for readability
-from sklearn.model_selection import GridSearchCV #Model used to optimize parameters of XGBoost model
-from sklearn.preprocessing import MinMaxScaler
+from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
+from colorama import Fore
+from sklearn.model_selection import GridSearchCV
 
 data = pd.read_csv("prepped_data.csv")
 
-#Code in this file is based on code provided in the workshop at the following link: https://github.com/wiscosac/wiscosac.github.io/blob/master/files/ML_Mania_Workshop.ipynb
+# RECORD = 0.18954 -> Random Tree (seedDiff, offRankDiff, defRankDiff, fgEff, ftRate, 3PG, FTPG, PDiffPG)
 
-#***** RECORD = 0.19773 -> seedDiff, offRankDiff, 3PG, FTPG, PDiffPG ******
-#***** SECOND = 0.2012 ->  seedDiff, offRankDiff, H/A SPLITS(3PG, FTPG, PDiffPG) ******
+# ********TODO****************
+# Add resume feature??
 
-#Features the model will be using
-features = ['Seed_Diff', 'offRankDiff', 'T1_HThreepg', 'T1_AThreepg', 'T2_HThreepg', 'T2_AThreepg', 'T1_HFTPG', 'T1_AFTPG', 'T2_HFTPG', 'T2_AFTPG', 'T1_HPDiffPG', 'T1_APDiffPG', 'T2_HPDiffPG', 'T2_APDiffPG']
+features = ['Seed_Diff', 'offRankDiff', 'defRankDiff', 'T1_fgEff', 'T2_fgEff', 'T1_ftRate', 'T2_ftRate', 'T1_Threepg', 'T2_Threepg', 'T1_FTPG', 'T2_FTPG', 'T1_PDiffPG', 'T2_PDiffPG']
 
-#Normalize the data
-scaler = MinMaxScaler()
-data[features] = scaler.fit_transform(data[features])
-
-#Train with tournament games 2003-2019, test with tournament games 2021-2024
 train = data[data['Season'] < 2020]
 test = data[data['Season'] > 2020]
 
-#Assign test and train values
 Xtrain = train[features]
-ytrain = (train['T1_Score'] > train['T2_Score']).astype(int) #We are predicting if T1 will beat T2 for each game in the data
+ytrain = (train['T1_Score'] > train['T2_Score']).astype(int)
 Xtest = test[features]
 ytest = (test['T1_Score'] > test['T2_Score']).astype(int)
 
 #Random Forest for Feature Selection
-rf = RandomForestClassifier(n_estimators=100, random_state=32)
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
 rf.fit(Xtrain, ytrain)
 
 feature_importance = pd.Series(rf.feature_importances_, index = Xtrain.columns)
-top_features = feature_importance.nlargest(11).index #Selects the n top features
+top_features = feature_importance.nlargest(10).index #Selects the 10 top features
 
 #Filter for only selected features
 Xtrain_selected = Xtrain[top_features]
@@ -46,7 +38,6 @@ m1 = XGBClassifier()
 m1.fit(Xtrain_selected, ytrain)
 predictions = m1.predict_proba(Xtest_selected)
 
-#Get the error score for this model
 output = pd.DataFrame(predictions[:,1], columns = ['Predictions'])
 output['Actual'] = ytest.astype(int).reset_index(drop=True)
 output["Score"] = (output["Actual"] - output["Predictions"])**2
@@ -58,23 +49,20 @@ print(Fore.BLUE + "********** PREDITCTION SCORE: " + Fore.YELLOW + str(output["S
 param_grid = {
     'max_depth': [3, 5, 7],
     'min_child_weight': [1, 3, 5],
-    'learning_rate': [0.1, 0.01, 0.001],
+    'learning_rate': [0.1, 0.01],
     'subsample': [0.6, 0.75, 0.9]
 }
 
-#Fit a gridSearch model to find the best parameters
 grid_search = GridSearchCV(estimator=m1, param_grid=param_grid, scoring='accuracy', cv=5)
 grid_search.fit(Xtrain_selected, ytrain)
 
 best_params = grid_search.best_params_
 
-#Train another XGBoost model, but this time with tuned parameters
 m2 = XGBClassifier(reg_alpha = 0.15, reg_lambda = 0.01, **best_params)
 
 m2.fit(Xtrain_selected, ytrain)
 predictions2 = m2.predict_proba(Xtest_selected)
 
-#Get our new error score
 output = pd.DataFrame(predictions2[:,1], columns = ['Predictions'])
 output['Actual'] = ytest.astype(int).reset_index(drop=True)
 output["Score"] = (output["Actual"] - output["Predictions"])**2
--- Processing.py
+++ Processing.py
@@ -1,10 +1,10 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
 
-#Code in this file is based on code provided in the workshop at the following link: https://github.com/wiscosac/wiscosac.github.io/blob/master/files/ML_Mania_Workshop.ipynb
+kenpom = pd.read_csv("Renamed_KenPom.csv") #Chat-GPT renamed teams to match spelling in MTeams.csv
+kenpom_off = pd.read_csv("INT _ KenPom _ Offense_Renamed.csv")
 
-kenpom = pd.read_csv("Renamed_KenPom.csv") #Chat-GPT renamed teams to match spelling in MTeams.csv, original data from March Madness Historical Data Set 2002-2025 on Kaggle
-#The following data is all from the March Learning Mania 2025 competition on Kaggle
 teams = pd.read_csv("MTeams.csv")
 season = pd.read_csv("MRegularSeasonDetailedResults.csv")
 tourney = pd.read_csv("MNCAATourneyCompactResults.csv")
@@ -13,56 +13,39 @@ seeds = pd.read_csv("MNCAATourneySeeds.csv")
 my_data = pd.DataFrame()
 year = 2003
 
-#Gets season averages for the given teamID and year
 def getAggregateStats(my_team, year):
     #my_team = 1104 #Alabama
     #year = 2021
     
-    #w_games = season.loc[((season['WTeamID'] == my_team)) & (season['Season'] == year)]
-    #l_games = season.loc[((season['LTeamID'] == my_team)) & (season['Season'] == year)]
-
-    away_w_games = season.loc[((season['WTeamID'] == my_team)) & (season['Season'] == year) & (season['WLoc'] != 'H')]
-    home_w_games = season.loc[((season['WTeamID'] == my_team)) & (season['Season'] == year) & (season['WLoc'] == 'H')]
-
-    away_l_games = season.loc[((season['LTeamID'] == my_team)) & (season['Season'] == year) & (season['WLoc'] != 'H')]
-    home_l_games = season.loc[((season['LTeamID'] == my_team)) & (season['Season'] == year) & (season['WLoc'] == 'H')]
+    w_games = season.loc[((season['WTeamID'] == my_team)) & (season['Season'] == year)]
+    l_games = season.loc[((season['LTeamID'] == my_team)) & (season['Season'] == year)]
     
+   # points_made = (w_games['WFGM3'].sum() * 3) + (l_games['LFGM3'].sum() * 3) + ((w_games['WFGM'].sum() - w_games['WFGM3'].sum()) * 2) + ((l_games['LFGM'].sum() - l_games['LFGM3'].sum()) * 2)
+   #posessions = w_games['WFGA'].sum() + w_games['WTO'].sum() + l_games['LFGA'].sum() + l_games['LTO'].sum()
+    gameCount = (len(w_games) + len(l_games))
+
+    threesPerG = (w_games['WFGM3'].sum() + l_games['LFGM3'].sum()) / gameCount  #Three Pointers Made / Game
+    ftpg = (w_games['WFTM'].sum() + l_games['LFTM'].sum()) / gameCount #Free Throws Made / Game
+    pdiffpg = ((w_games['WScore'].sum() - w_games['LScore'].sum()) + (l_games['LScore'].sum() - l_games['WScore'].sum())) / gameCount #Mean Point differential per game
    
-    home_gameCount = len(home_w_games) + len(home_l_games)
-    away_gameCount = len(away_w_games) + len(away_l_games)
+    return (threesPerG, ftpg, pdiffpg)
 
-    #gameCount = (len(w_games) + len(l_games))
-
-    #threesPerG = (w_games['WFGM3'].sum() + l_games['LFGM3'].sum()) / gameCount  #Three Pointers Made / Game
-    #ftpg = (w_games['WFTM'].sum() + l_games['LFTM'].sum()) / gameCount #Free Throws Made / Game
-    #pdiffpg = ((w_games['WScore'].sum() - w_games['LScore'].sum()) + (l_games['LScore'].sum() - l_games['WScore'].sum())) / gameCount #Mean Point differential per game
-
-    homeThreesPerG = (home_w_games['WFGM3'].sum() + home_l_games['LFGM3'].sum()) / home_gameCount
-    awayThreesPerG = (away_w_games['WFGM3'].sum() + away_l_games['LFGM3'].sum()) / away_gameCount
-
-    homeFtpg = (home_w_games['WFTM'].sum() + home_l_games['LFTM'].sum()) / home_gameCount
-    awayFtpg = (away_w_games['WFTM'].sum() + away_l_games['LFTM'].sum()) / away_gameCount
-
-    homePdiffpg = ((home_w_games['WScore'].sum() - home_w_games['LScore'].sum()) + 
-               (home_l_games['LScore'].sum() - home_l_games['WScore'].sum())) / home_gameCount
-    awayPdiffpg = ((away_w_games['WScore'].sum() - away_w_games['LScore'].sum()) + 
-               (away_l_games['LScore'].sum() - away_l_games['WScore'].sum())) / away_gameCount
-
-   
-    return (homeThreesPerG, awayThreesPerG, homeFtpg, awayFtpg, homePdiffpg, awayPdiffpg)
-
-#Get the regular season kenpom data we need for each team and each year 2003 - 2024, excluding 2020
 for y in range(2003, 2025):
     year = y
-    if(year != 2020): #There was no tournament this year
+    if(year != 2020):
         for x, z in zip(teams['TeamName'], teams['TeamID']):
-            if(kenpom.loc[(kenpom['Team'] == x) & (kenpom['Season'] == year)]["Adjusted Tempo"].any()):
+            if(kenpom.loc[(kenpom['Team'] == x) & (kenpom['Season'] == year)]["Adjusted Tempo"].any() and kenpom_off.loc[(kenpom_off['TeamName'] == x) & (kenpom_off['Season'] == year)]["eFGPct"].any()):
                 offRank = kenpom.loc[(kenpom['Team'] == x) & (kenpom['Season'] == year)]["Adjusted Offensive Efficiency Rank"].values[0]
+                defRank = kenpom.loc[(kenpom['Team'] == x) & (kenpom['Season'] == year)]["Adjusted Defensive Efficiency Rank"].values[0]
                 temp = kenpom.loc[(kenpom['Team'] == x) & (kenpom['Season'] == year)]["Adjusted Tempo"].values[0]
                 off = kenpom.loc[(kenpom['Team'] == x) & (kenpom['Season'] == year)]["Adjusted Offensive Efficiency"].values[0]
-                homeThreesPerG, awayThreesPerG, homeFtpg, awayFtpg, homePdiffpg, awayPdiffpg = getAggregateStats(z, year) #Add in the aggregate regular season stats
 
-                new_data = pd.DataFrame({'Year': y, 'ID': z, 'Team': x, 'offRank': offRank, 'offRating': off, 'tempo': temp, 'hThreesPG': homeThreesPerG, 'aThreesPG': awayThreesPerG, 'hFTPG': homeFtpg, 'aFTPG': awayFtpg, 'hPDiff': homePdiffpg, 'aPDiff': awayPdiffpg}, index = [x])
+                fgEff = kenpom_off.loc[(kenpom_off['TeamName'] == x) & (kenpom_off['Season'] == year)]["eFGPct"].values[0]
+                ftRate = kenpom_off.loc[(kenpom_off['TeamName'] == x) & (kenpom_off['Season'] == year)]["FTRate"].values[0]
+
+                threesPG, ftpg, pdiffpg = getAggregateStats(z, year)
+
+                new_data = pd.DataFrame({'Year': y, 'ID': z, 'Team': x, 'offRank': offRank, 'defRank': defRank, 'offRating': off, 'tempo': temp, 'fgEff': fgEff, 'ftRate': ftRate, 'threepg': threesPG, 'ftpg': ftpg, 'pDiffpg': pdiffpg}, index = [x])
 
                 if(not new_data.empty):
                     my_data = pd.concat([my_data, new_data])
@@ -77,7 +60,6 @@ for index, row in tourney.iterrows():
     lID = row['LTeamID']
     year = row['Season']
 
-    #Set up our data to have a column for each stat for both the winner and loser of each tournament game since 2003
     if(my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['offRating'].any() and my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['offRating'].any()):
         tourney.loc[index,'WOffRating'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['offRating'].values[0]
         tourney.loc[index,'LOffRating'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['offRating'].values[0]
@@ -85,32 +67,35 @@ for index, row in tourney.iterrows():
         tourney.loc[index,'WTempo'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['tempo'].values[0]
         tourney.loc[index,'LTempo'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['tempo'].values[0]
 
-        tourney.loc[index,'WHThreepg'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['hThreesPG'].values[0]
-        tourney.loc[index,'LHThreepg'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['hThreesPG'].values[0]
-        tourney.loc[index,'WAThreepg'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['aThreesPG'].values[0]
-        tourney.loc[index,'LAThreepg'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['aThreesPG'].values[0]
+        tourney.loc[index,'WfgEff'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['fgEff'].values[0]
+        tourney.loc[index,'LfgEff'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['fgEff'].values[0]
 
-        tourney.loc[index,'WHFTPG'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['hFTPG'].values[0]
-        tourney.loc[index,'LHFTPG'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['hFTPG'].values[0]
-        tourney.loc[index,'WAFTPG'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['aFTPG'].values[0]
-        tourney.loc[index,'LAFTPG'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['aFTPG'].values[0]
+        tourney.loc[index,'WftRate'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['ftRate'].values[0]
+        tourney.loc[index,'LftRate'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['ftRate'].values[0]
 
-        tourney.loc[index,'WHPDiffPG'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['hPDiff'].values[0]
-        tourney.loc[index,'LHPDiffPG'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['hPDiff'].values[0]
-        tourney.loc[index,'WAPDiffPG'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['aPDiff'].values[0]
-        tourney.loc[index,'LAPDiffPG'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['aPDiff'].values[0]
+        tourney.loc[index,'WThreepg'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['threepg'].values[0]
+        tourney.loc[index,'LThreepg'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['threepg'].values[0]
+
+        tourney.loc[index,'WFTPG'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['ftpg'].values[0]
+        tourney.loc[index,'LFTPG'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['ftpg'].values[0]
+
+        tourney.loc[index,'WPDiffPG'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['pDiffpg'].values[0]
+        tourney.loc[index,'LPDiffPG'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['pDiffpg'].values[0]
 
         tourney.loc[index,'WOffRank'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['offRank'].values[0]
         tourney.loc[index,'LOffRank'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['offRank'].values[0]
+
+        tourney.loc[index,'WDefRank'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == wID)]['defRank'].values[0]
+        tourney.loc[index,'LDefRank'] = my_data.loc[(my_data['Year'] == year) & (my_data['ID'] == lID)]['defRank'].values[0]
 
 print(tourney.head())
 
 prepped_data = tourney.dropna()
 print(prepped_data.shape)
 
-#Replace the W and L with T_1 and T_2 and make 2 copies of each game so that the model predicts off features and not just the position/names of the variables
+
 def prepare_data(df):
-    dfswap = df[['Season', 'DayNum', 'LTeamID', 'LScore', 'WTeamID', 'WScore', 'WLoc', 'NumOT', 'WOffRating', 'LOffRating', 'WOffRank', 'LOffRank', 'WTempo', 'LTempo', 'WHThreepg', 'WAThreepg', 'LHThreepg', 'LAThreepg', 'WHFTPG', 'WAFTPG', 'LHFTPG', 'LAFTPG', 'WHPDiffPG', 'WAPDiffPG', 'LHPDiffPG', 'LAPDiffPG']]
+    dfswap = df[['Season', 'DayNum', 'LTeamID', 'LScore', 'WTeamID', 'WScore', 'WLoc', 'NumOT', 'WOffRating', 'LOffRating', 'WOffRank', 'LOffRank', 'WDefRank', 'LDefRank', 'WTempo', 'LTempo', 'WfgEff', 'LfgEff', 'WftRate', 'LftRate', 'WThreepg', 'LThreepg', 'WFTPG', 'LFTPG', 'WPDiffPG', 'LPDiffPG']]
   
     df.columns = [x.replace('W','T1_').replace('L','T2_') for x in list(df.columns)]
     dfswap.columns = [x.replace('L','T1_').replace('W','T2_') for x in list(dfswap.columns)]
@@ -128,6 +113,7 @@ prepped_data = prepare_data(prepped_data)
 #prepped_data['offRatingDiff'] = prepped_data['T1_OffRating'] - prepped_data['T2_OffRating']
 #prepped_data['tempoDiff'] = prepped_data['T1_Tempo'] - prepped_data['T2_Tempo']
 prepped_data['offRankDiff'] = prepped_data['T1_OffRank'] - prepped_data['T2_OffRank']
+prepped_data['defRankDiff'] = prepped_data['T1_DefRank'] - prepped_data['T2_DefRank']
 
 
 #Apply seeds
@@ -143,7 +129,6 @@ prepped_data['Seed_Diff'] = prepped_data['T1_seed'] - prepped_data['T2_seed']
 
 print(prepped_data.head())
 
-#Save the processed data for use with the model
 prepped_data.to_csv('prepped_data.csv', index=False)
 
 
